7d5a7e0147a7da033edd87ea43b4f4dfeab25fc6
hange1984 <hange1984@126.com>
2018-02-07 15:08:13 +0800

    正则表达式，确定邮件是否满足表达式

--- 1234.py
+++ 1234.py
@@ -1,8 +1,12 @@
-#-*-coding:utf-8 -*-
+# -*-coding:utf-8 -*-
+
 '''
-  正则表达式 确定邮件是否满足正则表达式的要求。
+    正则表达式 确定邮件是否满足正则表达式的要求。
 '''
+
 import re
+
+
 def is_valid_email(addr):
     if re.match('[\w+.-]+@[\w.-]+.\w{3}',addr):
         return True
